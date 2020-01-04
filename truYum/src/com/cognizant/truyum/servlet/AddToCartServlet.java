01c1e98555f27c2cc69aa08f9a4f9101171c62db
Preeya123 <preeyankamujagond@gmail.com>
2020-01-04 09:47:52 +0530

    Servlet & JSP

---
+++ truYum/src/com/cognizant/truyum/servlet/AddToCartServlet.java
@@ -0,0 +1,50 @@
+package com.cognizant.truyum.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cognizant.truyum.dao.CartDao;
+import com.cognizant.truyum.dao.CartDaoCollectionImpl;
+
+/**
+ * Servlet implementation class AddToCartServlet
+ */
+@WebServlet({ "/AddToCartServlet", "/AddToCart" })
+public class AddToCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddToCartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CartDao cartDao = new CartDaoCollectionImpl();
+		long id=Long.parseLong(request.getParameter("id"));
+		cartDao.addCartItem(1,id);
+		request.setAttribute("msg","Item added successfully");
+		RequestDispatcher rd=request.getRequestDispatcher("ShowMenuItemListCustomer");
+		rd.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
